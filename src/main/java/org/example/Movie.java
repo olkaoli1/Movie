97809fbe7eb6abcd833c2a0de2f6d3eb72be9baf
Olga <oli.samsonowa@yandex.ru>
2024-11-27 01:06:13 +0300

    Add MovieManager implementation with tests

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -1,17 +1,32 @@
 package com.example;
 
+import java.util.Objects;
+
 public class Movie {
-    private String title;
+    private String name;
 
-    public Movie(String title) {
-        this.title = title;
+    public Movie(String name) {
+        this.name = name;
     }
 
-    public String getTitle() {
-        return title;
+    public String getName() {
+        return name;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return Objects.equals(name, movie.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
